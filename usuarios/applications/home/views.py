f78f219195f5886a447c57915054fe553cf0e886
Juan Sandino <jsandino@aimservices.tech>
2020-09-03 17:18:49 -0600

    refactor feat: implementar un Mixin (LoginRequiredMixin)  para ecitar que un usuario no logueado acceda a la pantalla principal

--- usuarios/applications/home/views.py
+++ usuarios/applications/home/views.py
@@ -1,20 +1,27 @@
 import datetime
 #
 from django.shortcuts import render
+#
+from django.contrib.auth.mixins import LoginRequiredMixin
+#
+from django.urls import reverse_lazy
+
 # Views Generic
 from django.views.generic import TemplateView
 
-
-class HomePage(TemplateView):
+# LoginRequiredMixin, indicara que necesitamos estar logueado para visualizar esta vista
+class HomePage(LoginRequiredMixin, TemplateView):
     
     template_name = "home/index.html"
+    # Si el usuario no esta logueado se le redigira a la siguiente ruta
+    login_url = reverse_lazy('users_app:login')
 
     def get_context_data(self, **kwargs):
         context = super(HomePage, self).get_context_data(**kwargs)
         context['titulo'] = 'Panel de usuario'
         context['sub_titulo'] = 'Bienvenidos'
         return context
-        
+
 # Un Mixon personalizado siempre debe heredar de object
 class FechaMixin(object):
      
