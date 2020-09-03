b1a9a3732455be9f12e965d52a1a3bd756e40413
Juan Sandino <jsandino@aimservices.tech>
2020-09-03 17:06:16 -0600

    chore: probando un Mixin personalizado

--- usuarios/applications/home/urls.py
+++ usuarios/applications/home/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'home_app'
 
 urlpatterns = [
-    path('home/', views.HomePage.as_view(), name ='your-house')
+    path('home/', views.HomePage.as_view(), name ='your-house'),
+    path('mixin/', views.TemplatePruebaMixin.as_view(), name='mixin')
 ]
--- usuarios/applications/home/views.py
+++ usuarios/applications/home/views.py
@@ -1,3 +1,5 @@
+import datetime
+#
 from django.shortcuts import render
 # Views Generic
 from django.views.generic import TemplateView
@@ -12,4 +14,17 @@ class HomePage(TemplateView):
         context['titulo'] = 'Panel de usuario'
         context['sub_titulo'] = 'Bienvenidos'
         return context
+        
+# Un Mixon personalizado siempre debe heredar de object
+class FechaMixin(object):
+     
+    def get_context_data(self, **kwargs):
+        context = super(FechaMixin, self).get_context_data(**kwargs)
+        context['fecha'] = datetime.datetime.now()
+        return context
+
+class TemplatePruebaMixin(FechaMixin, TemplateView):
+
+    template_name = 'home/mixin.html'
+
     
